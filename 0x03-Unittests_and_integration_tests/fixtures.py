3aa9681286343217abdbb94500c0b6b13eff4e60
Dedolaa <adedolapobakare16@gmail.com>
2025-07-17 16:33:57 +0100

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 
+org_payload = {
+    "login": "google",
+    "id": 1,
+    # other fields as necessary...
+}
+
+repos_payload = [
+    {
+        "id": 1,
+        "name": "repo1",
+        "license": {"key": "apache-2.0"}
+    },
+    {
+        "id": 2,
+        "name": "repo2",
+        "license": {"key": "mit"}
+    }
+]
+
+expected_repos = ["repo1", "repo2"]
+
+apache2_repos = ["repo1"]
+
 TEST_PAYLOAD = [
   (
     {"repos_url": "https://api.github.com/orgs/google/repos"},
