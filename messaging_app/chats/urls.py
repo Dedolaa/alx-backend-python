642bacd790b5b8ceac4c6d3f4b871b9bc30f2bdd
Dedolaa <adedolapobakare16@gmail.com>
2025-07-20 14:39:39 +0100

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -10,3 +10,11 @@ router.register(r'messages', MessageViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+
+convo_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = router.urls + convo_router.urls
