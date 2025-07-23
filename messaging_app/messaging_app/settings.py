6ce3c1a80b7a932f44b28a614b92358b1be2da59
Dedolaa <adedolapobakare16@gmail.com>
2025-07-23 19:39:12 +0100

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -176,3 +176,9 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,  
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',  # ← add this explicitly
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
