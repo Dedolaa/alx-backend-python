ea74d665f1410919f74b446f11e8aea5fb0c66a8
Dedolaa <adedolapobakare16@gmail.com>
2025-07-23 19:18:28 +0100

    Update filters.py

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -9,3 +9,12 @@ class MessageFilter(django_filters.FilterSet):
     class Meta:
         model = Message
         fields = ['user', 'created_at__gte', 'created_at__lte']
+
+class MessageFilter(django_filters.FilterSet):
+    sender = django_filters.CharFilter(field_name='sender__username', lookup_expr='icontains')
+    recipient = django_filters.CharFilter(field_name='recipient__username', lookup_expr='icontains')
+    timestamp = django_filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Message
+        fields = ['sender', 'recipient', 'timestamp']
