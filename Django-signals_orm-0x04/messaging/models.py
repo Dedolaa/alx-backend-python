0c7791780a5da909cc97ca4697b8cff5fdbe45f7
Dedolaa <adedolapobakare16@gmail.com>
2025-07-31 15:54:22 +0100

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -13,6 +13,7 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages')
     read = models.BooleanField(default=False)
     parent_message = models.ForeignKey(
         'self',
