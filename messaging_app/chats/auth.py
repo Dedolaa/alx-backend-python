ea8510a69b84a8062bf7467176fa29d17bbe0c0c
Adedolapo Bakare <adedolapobakare16@gmail.com>
2025-08-13 18:51:04 +0100

    Add messaging_app folder with Kubernetes deployments and scripts

--- messaging_app/chats/auth.py
+++
@@ -1,8 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """
-    Custom JWT Authentication class if you need to add extra behavior.
-    For now, it just uses the standard JWTAuthentication.
-    """
-    pass
