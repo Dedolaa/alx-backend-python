490ff7a0c842d301c1a0028c54a2c30f50716800
Adedolapo Bakare <adedolapobakare16@gmail.com>
2025-08-21 13:33:22 +0100

    Fix: Use pytest built-in JUnit functionality

--- messaging_app/tests/test_example.py
+++ messaging_app/tests/test_example.py
@@ -1,7 +1,8 @@
-def test_example():
-    """Simple test example"""
+def test_addition():
     assert 1 + 1 == 2
 
-def test_another_example():
-    """Another simple test"""
+def test_string():
     assert "hello".upper() == "HELLO"
+
+def test_list():
+    assert len([1, 2, 3]) == 3
